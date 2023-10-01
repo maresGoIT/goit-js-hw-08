01c5426b3b1e43ef788a0630a88ff7c33045197a
CristinaBente <128289401+CristinaBente@users.noreply.github.com>
2023-10-01 11:43:53 +0300

    init-v

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
+function loadSavedTime(vimeo) {
+  const savedTime = localStorage.getItem("videoplayer-current-time");
+  if (savedTime) {
+    vimeo.setCurrentTime(parseFloat(savedTime));
+  }
+}
+function handleTimeUpdate(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem("videoplayer-current-time", currentTime);
+}
+function initializePlayer() {
+  const iframe = document.querySelector("iframe");
+  const player = new Vimeo.Player(iframe);
 
+  loadSavedTime(player);
+
+  const throttledTimeUpdate = throttle(handleTimeUpdate, 1000);
+
+  player.on("timeupdate", throttledTimeUpdate);
+
+  player.on("play", function () {
+    console.log("played the video!");
+  });
+
+  player.getVideoTitle().then(function (title) {
+    console.log("title:", title);
+  });
+}
+
+initializePlayer();
